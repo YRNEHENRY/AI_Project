9611a2e1c24f624bf4b96adebc39ff333976f1bd
BoBordelique <dbez.henry@gmail.com>
2022-09-24 17:55:55 +0200

    Update jsBase.js

--- game_app/static/jsBase.js
+++ game_app/static/jsBase.js
@@ -1,17 +1,10 @@
 let isWhiteMode = true;
 
 function changeStyleMode(){
-
         let lienActuel = document.children[0].children[0].children[3].href
         let tabLienActuel = lienActuel.split("/")
 
-        if(isWhiteMode){
-            tabLienActuel[4] = 'styleDark.css'
-            isWhiteMode = false
-        }
-        else{
-            tabLienActuel[4] = 'styleWhite.css'
-            isWhiteMode = true
-        }
+        (isWhiteMode ? tabLienActuel[4] = 'styleDark.css' : tabLienActuel[4] = 'styleWhite.css')
+        isWhiteMode = !isWhiteMode
         document.children[0].children[0].children[3].href = tabLienActuel.join("/")
 }
