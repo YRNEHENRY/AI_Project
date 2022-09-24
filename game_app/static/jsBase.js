290df365faa024efb0ae0ef3083e61913f59801b
AurelienSomme <aure.somme@hotmail.com>
2022-09-24 21:03:12 +0200

    Update jsBase.js

--- game_app/static/jsBase.js
+++ game_app/static/jsBase.js
@@ -4,7 +4,7 @@ function changeStyleMode(){
         let lienActuel = document.children[0].children[0].children[3].href
         let tabLienActuel = lienActuel.split("/")
 
-        (isWhiteMode ? tabLienActuel[4] = 'styleDark.css' : tabLienActuel[4] = 'styleWhite.css')
+        isWhiteMode ? (tabLienActuel[4] = 'styleDark.css') : (tabLienActuel[4] = 'styleWhite.css')
         isWhiteMode = !isWhiteMode
         document.children[0].children[0].children[3].href = tabLienActuel.join("/")
 }
