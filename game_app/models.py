6d5ce4c5aec67dc01cfa26a8edf463bb6a37ab2d
BoBordelique <dbez.henry@gmail.com>
2022-10-25 10:20:42 +0200

    SQLAlchemy : Updating Models
    Updating models with SQLAlchemy (still not finish)

--- game_app/models.py
+++ game_app/models.py
@@ -1,44 +1,59 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
 
-class User:
+class User(db.Model):
+    __tablename__ = "user"
+    __abstract__ = True
 
-    def __init__(self, login):
-        self.login = login
+    id = db.Column(db.String(), primary_key = True)
 
 
 class Human (User):
+    __tablename__ = "human"
+    __table_args__ = (UniqueConstraint('login'))
 
-    def __init__(self, login, password, email, name, first_name):
-        super().__init__(login)
+    login = db.Column(db.string(25), )
+    password = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    name = db.Column(db.String(25))
+    first_name = db.Column(db.String(25))
+
+    def __init__(self, password, email, name, first_name):
+        #super().__init__()
         self.password = password
         self.email = email
         self.name = name
         self.first_name = first_name
 
 class AI (User):
+    __tablename__ = "ai"
 
-    def __init__(self, login):
-        super().__init__(login)
+    def __init__(self):
+        pass
 
-class Player:
+class Board(db.Model):
+    __tablename__ = "board"
 
-    def __init__(self, position):
-        self.position = position
+    id = db.Column(db.Integer, primary_key = True)
+    size = db.Column(db.Integer, nullable = False)
+    state_board = db.Column(db.String(36))
+    turn = db.Column(db.Integer)
+    position_p1 = db.Column(db.String(2))
+    position_p2 = db.Column(db.String(2))
 
-class Board:
-
-    def __init__(self, id, size, state_board, turn):
-        self.id = id
+    def __init__(self, size, state_board, turn, position_p1, position_p2):
         self.size = size
         self.state_board = state_board
         self.turn = turn
+        self.position_p1 = position_p1
+        self.position_p2 = position_p2
 
 class Position_history:
-
+    __tablename__ = "position_history"
     def __init__(self) -> None:
         pass
