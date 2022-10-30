863716afb5dee638270709cc586be06e3b1eda80
BoBordelique <dbez.henry@gmail.com>
2022-10-30 12:54:41 +0100

    Update models.py
    Adding imports
    
    Adding foreign keys
    Adding Columns
    
    AI : pass

--- game_app/models.py
+++ game_app/models.py
@@ -1,46 +1,61 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, UniqueConstraint
-from sqlalchemy.orm import relationship
+import ast
+import logging as lg
+
+from sqlalchemy import Column, ForeignKey, Integer
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = "user"
-    __abstract__ = True
+def init_db():
+    db.drop_all()
+    db.create_all()
 
-    id = db.Column(db.Integer, primary_key = True)
-    #games = relationship("Board")
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(25), nullable = False, unique = True)
+    nb_wins = db.Column(db.Integer, nullable = False)
+    nb_defeats = db.Column(db.Integer, nullable = False)
+
+    games = db.relationship('Boards', backref='player') # multiple foreign key to board (many to one)
+
+    def __init__(self,login):
+        self.login = login
 
 
-class Human (User):
-    __tablename__ = "human"
-    #__table_args__ = (UniqueConstraint('login'))
+class Humans (db.Model):
+    __tablename__ = "humans"
 
-    login = db.Column(db.String(25), nullable = False)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     password = db.Column(db.String(50), nullable = False)
-    email = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
     name = db.Column(db.String(25), nullable = False)
     first_name = db.Column(db.String(25), nullable = False)
 
-    def __init__(self, login, password, email, name, first_name):
-        super().__init__()
-        self.login = login
+    user = db.Column(db.Integer,db.ForeignKey('user.id')) # foreign key to user
+
+    def __init__(self, password, email, name, first_name):
         self.password = password
         self.email = email
         self.name = name
         self.first_name = first_name
 
-class AI (User):
-    __tablename__ = "ai"
+class AIs (db.Model):
+    __tablename__ = "ais"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+
+    user = db.Column(db.Integer,db.ForeignKey('user.id')) # foreign key to user
 
     def __init__(self):
         pass
 
-class Board(db.Model):
-    __tablename__ = "board"
+class Boards(db.Model):
+    __tablename__ = "boards"
 
     id = db.Column(db.Integer, primary_key = True)
     size = db.Column(db.Integer, nullable = False)
@@ -49,18 +64,19 @@ class Board(db.Model):
     position_p1 = db.Column(db.String(2), nullable = False)
     position_p2 = db.Column(db.String(2), nullable = False)
 
-    #player1_id = Column(Integer, ForeignKey("user.id"))
-    #player2_id = Column(Integer, ForeignKey("user.id"))
+    player_1 = db.Column(Integer, ForeignKey("users.id")) # foreign key to user
+    player_2 = db.Column(Integer, ForeignKey("users.id")) # foreign key to user
 
-    def __init__(self, size, state_board, turn, position_p1, position_p2):
+    def __init__(self, size, state_board, turn, position_p1, position_p2, player_1, player_2):
         self.size = size
-        self.state_board = state_board
+        self.state_board = state_board # à la création, faire un algorithme qui remplie le tableau de 0 * X cellules
         self.turn = turn
         self.position_p1 = position_p1
         self.position_p2 = position_p2
+        self.player_1 = player_1
+        self.player_2 = player_2
 
     def update_state(self):
-
         if self.turn == 1:
             x = self.position_p1[0]
             y = self.position_p1[1]
