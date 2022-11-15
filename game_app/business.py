f67719e6e4c3ad1430761e71c5c97032e2cf31a2
AurelienSomme <aure.somme@hotmail.com>
2022-11-15 13:52:31 +0100

    Fixing model/Adding Businnes

--- game_app/business.py
+++ game_app/business.py
@@ -1,62 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from game_app.ai import AIs
-from sqlalchemy import Column, ForeignKey, Integer
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
-db = SQLAlchemy(app)
-
-def init_db():
-    db.drop_all()
-    db.create_all()
-
-class Users(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    username = db.Column(db.String(25), nullable = False, unique = True)
-    nb_wins = db.Column(db.Integer, nullable = False)
-    nb_defeats = db.Column(db.Integer, nullable = False)
-
-    #games = db.relationship('Boards', backref='player') # multiple foreign key to board (many to one)
-
-    def __init__(self,login):
-        self.login = login
-
-
-class Humans (db.Model):
-    __tablename__ = "humans"
-
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    password = db.Column(db.String(50), nullable = False)
-    email = db.Column(db.String(50), nullable = False, unique = True)
-    name = db.Column(db.String(25), nullable = False)
-    first_name = db.Column(db.String(25), nullable = False)
-
-    user = db.Column(db.Integer,db.ForeignKey('user.id')) # foreign key to user
-
-    def __init__(self, password, email, name, first_name):
-        self.password = password
-        self.email = email
-        self.name = name
-        self.first_name = first_name
-
-
-        
-
-class Boards(db.Model):
-    __tablename__ = "boards"
-
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    size = db.Column(db.Integer, nullable = False)
-    state_board = db.Column(db.String(36), nullable = False)
-    turn = db.Column(db.Integer, nullable = False)
-    position_p1 = db.Column(db.String(2), nullable = False)
-    position_p2 = db.Column(db.String(2), nullable = False)
-
-    player_1 = db.Column(Integer, ForeignKey("users.id")) # foreign key to user
-    player_2 = db.Column(Integer, ForeignKey("users.id")) # foreign key to user
+class Boards():
 
     def __init__(self, size, player_1, player_2):
         self.size = size
@@ -64,10 +6,10 @@ class Boards(db.Model):
         self.turn = 1
         self.position_p1 = [0,0]
         self.position_p2 = [self.size-1, self.size-1]
-        #self.positions = [self.position_p1, self.position_p2]
+        self.positions = [self.position_p1, self.position_p2]
         self.player_1 = player_1
         self.player_2 = player_2
-        #self.players = [player_1, player_2]
+        self.players = [player_1, player_2]
 
     def play(self):
         is_done = self.is_done()[0]
@@ -275,9 +217,3 @@ class Boards(db.Model):
         elif nb_1 < nb_2:
             winner = self.player_2.first_name if not isinstance(self.player_2, AIs) else "AI n°2"
         return is_done, winner
-
-
-#class Position_history(db.Model):
-#    __tablename__ = "position_history"
-#    def __init__(self) -> None:
-#        pass
