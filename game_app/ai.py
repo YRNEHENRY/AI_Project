a2dffac71546faf4f06babdc163a99cc4e8f5fc2
BoBordelique <dbez.henry@gmail.com>
2022-12-07 11:02:33 +0100

    Update ai.py

--- game_app/ai.py
+++ game_app/ai.py
@@ -10,18 +10,13 @@ class AI ():
         self.eps = 0.95
         self.Q_table = []
 
-
-
     def set_board(self, board):
         self.board = board
 
     def exploration_step(self, position):
         possible_move = self.board.get_possible_move(position)
-        
         i_random = random.randint(0, len(possible_move) - 1)
-
         self.board.positions[self.board.turn - 1] = possible_move[i_random]
-
         self.board.update_state()
 
 
@@ -33,16 +28,12 @@ class AI ():
         for i in range(len(actions)):
             action = actions[i]
 
-
-
         self.board.update_state()
 
     def get_state(self,action,state):
         return 
     
-
     def play(self, position, state):
-
         #if random.uniform(0, 1) < self.eps:
         if True:
             self.exploration_step(position)
